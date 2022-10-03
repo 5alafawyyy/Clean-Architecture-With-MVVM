2ec77b3fdfdc3135d2758a093ccc468772927e00
Ahmed Khallaf <[ahmedkhallaf2098@gmail.com]>
2022-10-03 19:43:14 +0200

    Data Layer Add API Service Client Retrofit

---
+++ lib/app/constants.dart
@@ -0,0 +1,4 @@
+class Constants {
+  static const String baseUrl = 'https://mrpen10.mocklab.io';
+  static const String login = '/customer/login';
+}
---
+++ lib/data/network/app_api.dart
@@ -0,0 +1,16 @@
+import 'package:clean_architecture_with_mvvm/app/constants.dart';
+import 'package:clean_architecture_with_mvvm/data/response/responses.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
+part 'app_api.g.dart';
+
+@RestApi(baseUrl: Constants.baseUrl)
+abstract class AppServiceClient {
+  factory AppServiceClient(Dio dio, {String baseUrl}) = _AppServiceClient;
+
+  @POST(Constants.login)
+  Future<AuthenticationResponse> login(
+    @Field("email") String email,
+    @Field("password") String password,
+  );
+}
