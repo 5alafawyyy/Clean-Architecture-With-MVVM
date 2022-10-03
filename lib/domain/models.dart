6261a5069bf99fafa7e649e47e0348d1ee717ce0
Ahmed Khallaf <[ahmedkhallaf2098@gmail.com]>
2022-10-04 01:50:17 +0200

    Application Layer Creating Extenstions

--- lib/domain/models.dart
+++ lib/domain/models.dart
@@ -15,10 +15,45 @@ class SliderViewObject {
   SliderObject sliderObject;
   int numOfSlides;
   int currentIndex;
-  
+
   SliderViewObject({
     required this.sliderObject,
     required this.numOfSlides,
     required this.currentIndex,
   });
+}
+
+// login models
+class Customer {
+  String id;
+  String name;
+  int numOfNotifications;
+
+  Customer(
+    this.id,
+    this.name,
+    this.numOfNotifications,
+  );
+}
+
+class Contacts {
+  String phone;
+  String email;
+  int link;
+
+  Contacts(
+    this.phone,
+    this.email,
+    this.link,
+  );
+}
+
+class Authentication {
+  Customer customer;
+  Contacts contacts;
+
+  Authentication(
+    this.customer,
+    this.contacts,
+  );
 }
